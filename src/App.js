47ba4ef78a7b95a49d71e92d6f79827e334ea0c6
kawarhossain <38612699+kawarhossain@users.noreply.github.com>
2019-04-06 17:14:34 +0600

    this is pure-redux a simple example

--- src/App.js
+++ src/App.js
@@ -1,24 +1,65 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { createStore } from 'redux';
 
 class App extends Component {
   render() {
+
+    // Initial State
+    const initState = {
+      default : 'this is default value'
+    }
+
+    // @reducer
+    // for create a state
+    const reducer = (initState, action) => {
+      switch (action.type) {
+        case 'React':
+          return {
+            ...initState,
+            love: 'i love React'
+          }
+
+        case 'Example':
+          return {
+            ...initState,
+            payload: action.payload
+          }
+
+        default: 
+          return initState;
+      }
+    }
+
+    // Data Store
+    let store = createStore(reducer);
+
+    // @Update Data Collect
+    // After Dispatch
+    let updateState = {};
+
+    // store  subscribe
+    // you should must subscribe to take data from store
+    store.subscribe(() => {
+      updateState = store.getState();
+    });
+
+    // @actions passess to update state
+    store.dispatch({ type: 'C'});  // this action give us a null object
+    store.dispatch({ type: 'D'});  // this action give us a null object
+    store.dispatch({ type: 'React' }); 
+    store.dispatch({ type: 'Example' , payload : 'this is simple example of pure REDUX'});
+
+
+       
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+
+          <h3>{updateState.love.toUpperCase()}</h3>
+          <h3>{updateState.payload}</h3>
         </header>
       </div>
     );
